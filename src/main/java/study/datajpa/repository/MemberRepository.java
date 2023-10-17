a6bf88d7d8e63f4311baa29aa2dd6d0991913ffe
Leehyoju97 <dlgywn2420@gmail.com>
2023-10-18 02:08:31 +0900

    스프링 데이터 jpa기반 테스트

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,23 +1,7 @@
 package study.datajpa.repository;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import study.datajpa.entity.Member;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
-@Repository
-public class MemberRepository {
-
-    @PersistenceContext
-    private EntityManager em;
-
-    public Member save(Member member) {
-        em.persist(member);
-        return member;
-    }
-
-    public Member find(Long id) {
-        return em.find(Member.class, id);
-    }
+public interface MemberRepository extends JpaRepository<Member, Long> {
 }
